cb3db75e19a0f076b9decfa78d60c5eff73ba696
SangBejo <120177338+SangBejoo@users.noreply.github.com>
2024-08-25 13:37:01 +0700

    Add files via upload

---
+++ AyubSubagiya_2107412021/inc/container.php
@@ -0,0 +1,12 @@
+</head>
+<body class="bg-gradient bg-dark bg-opacity-25">
+<nav role="navigation" class="navbar navbar-dark navbar-static-top bg-primary bg-gradient">
+    <div class="container">
+      <div class="navbar-header">
+        <p> Ayub Subagiya 2107412021 UAS Project Web Lanjutan</p>
+      </div>
+    </div>
+  </nav>
+	
+	<div class="container">
+	
